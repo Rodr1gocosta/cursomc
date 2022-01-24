5505eb6b909ec23ad6181bb72cab54f2014e74ec
rodrigo.costa <rodrigo.costa@basis.com.br>
2022-01-24 14:49:49 -0300

    Projeto criado

---
+++ src/test/java/com/rodrigocosta/cursojava1/Cursojava1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodrigocosta.cursojava1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cursojava1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
