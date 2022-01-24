8a27f641e0c5e3df23e6c58cdc142f6aac8d9615
rodrigo.costa <rodrigo.costa@basis.com.br>
2022-01-24 17:23:55 -0300

    Criando operacao de instanciacao

--- src/main/java/com/rodrigocosta/cursojava1/Cursojava1Application.java
+++ src/main/java/com/rodrigocosta/cursojava1/Cursojava1Application.java
@@ -1,13 +1,31 @@
 package com.rodrigocosta.cursojava1;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class Cursojava1Application {
+import com.rodrigocosta.cursojava1.domain.Categoria;
+import com.rodrigocosta.cursojava1.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class Cursojava1Application implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Cursojava1Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
