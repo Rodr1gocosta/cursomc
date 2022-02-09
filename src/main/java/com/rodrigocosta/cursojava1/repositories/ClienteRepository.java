4f34a36d151b91028987bde0de891832d398cd42
rodrigo.costa <rodrigo.costa@basis.com.br>
2022-02-09 14:48:01 -0300

    Validacao customizada email

--- src/main/java/com/rodrigocosta/cursojava1/repositories/ClienteRepository.java
+++ src/main/java/com/rodrigocosta/cursojava1/repositories/ClienteRepository.java
@@ -2,10 +2,13 @@ package com.rodrigocosta.cursojava1.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.rodrigocosta.cursojava1.domain.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
-	
+
+	@Transactional(readOnly=true)
+	Cliente findByEmail(String email);
 }
--- src/main/java/com/rodrigocosta/cursojava1/services/validation/ClienteInsertValidation.java
+++ src/main/java/com/rodrigocosta/cursojava1/services/validation/ClienteInsertValidation.java
@@ -6,12 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.rodrigocosta.cursojava1.domain.Cliente;
 import com.rodrigocosta.cursojava1.domain.enums.TipoCliente;
 import com.rodrigocosta.cursojava1.dto.ClienteNewDTO;
+import com.rodrigocosta.cursojava1.repositories.ClienteRepository;
 import com.rodrigocosta.cursojava1.resources.exception.FieldMessage;
 import com.rodrigocosta.cursojava1.services.validation.utils.BR;
 
 public class ClienteInsertValidation implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -26,6 +34,11 @@ public class ClienteInsertValidation implements ConstraintValidator<ClienteInser
 
 		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+		}
+		
+		Cliente aux = clienteRepository.findByEmail(objDto.getEmail());
+		if (aux != null) {
+			list.add(new FieldMessage("email", "Email já existente"));
 		}
 		
 		for (FieldMessage e : list) {
