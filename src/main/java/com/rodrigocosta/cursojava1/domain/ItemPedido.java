87042fe7be74fcb38a3af82a6d8b268c893344c8
rodr1gocosta <rodrigomcosta20@gmail.com>
2022-02-25 15:04:26 -0300

    Inserindo PEDIDO

--- src/main/java/com/rodrigocosta/cursojava1/domain/ItemPedido.java
+++ src/main/java/com/rodrigocosta/cursojava1/domain/ItemPedido.java
@@ -34,6 +34,7 @@ public class ItemPedido implements Serializable {
 	
 	public double getSubTotal() {
 		return (preco - desconto) * quantidade;
+		
 	}
 	
 	@JsonIgnore
@@ -41,8 +42,16 @@ public class ItemPedido implements Serializable {
 		return id.getPedido();
 	}
 	
+	public void setPedido(Pedido pedido) {
+		id.setPedido(pedido);
+	}
+	
 	public Produto getProduto() {
 		return id.getProduto();
+	}
+	
+	public void setProduto(Produto produto) {
+		id.setProduto(produto);
 	}
 
 	public ItemPedidoPK getId() {
